3763b48f86b72896d812760f0a1588118430d83a
kazakov.ad1231 <kazakov.ad1231@COM407-R2>
2023-12-18 10:22:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using MyLib;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Class1.Add(100, 200));
+    }
+}
---
+++ MyLib/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyLib
+{
+    public class Class1
+    {
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, int options);
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+
